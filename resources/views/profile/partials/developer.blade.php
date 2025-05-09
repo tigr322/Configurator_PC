74f750aaead7f04453e901d01b2871a8cc0f7dd0
Тигран Адамян <tigranadaman@MacBook-Air-Tigran.local>
2025-05-09 22:58:14 +0300

    Доработа профиля

--- resources/views/profile/partials/developer.blade.php
+++ resources/views/profile/partials/developer.blade.php
@@ -26,12 +26,15 @@
           <svg class="absolute top-0 left-0 h-18 w-18 -translate-x-8 -translate-y-24 transform text-indigo-200 opacity-50" stroke="currentColor" fill="none" viewBox="0 0 144 144" aria-hidden="true">
             <path stroke-width="2" d="M41.485 15C17.753 31.753 1 59.208 1 89.455c0 24.664 14.891 39.09 32.109 39.09 16.287 0 28.386-13.03 28.386-28.387 0-15.356-10.703-26.524-24.663-26.524-2.792 0-6.515.465-7.446.93 2.327-15.821 17.218-34.435 32.11-43.742L41.485 15zm80.04 0c-23.268 16.753-40.02 44.208-40.02 74.455 0 24.664 14.891 39.09 32.109 39.09 15.822 0 28.386-13.03 28.386-28.387 0-15.356-11.168-26.524-25.129-26.524-2.792 0-6.049.465-6.98.93 2.327-15.821 16.753-34.435 31.644-43.742L121.525 15z" />
           </svg>
-          <blockquote class="relative">
-            <div class="text-2xl font-medium leading-9">
-              <p>Код — это как шутка. Если нужно объяснять, значит, она плоха. - Разработчик Тигран Адамян</p>
-            </div>
-            
-          </blockquote>
+          <div class="max-w-xl mx-auto text-center mt-10">
+            <h2 class="text-3xl font-bold mb-4">Адамян Тигран — Разработчик</h2>
+            <blockquote class="relative p-6 rounded-2xl shadow-md">
+              <div class="text-2xl font-medium leading-9">
+                <p>Идеальный компьютер — не тот, что купил, а тот, что собрал сам.</p>
+              </div>
+            </blockquote>
+          </div>
+          
         </div>
       </div>
     </div>
