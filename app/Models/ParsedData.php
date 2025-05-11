1c49a0781fde81de96b73822a99f74dea1b60b7f
Тигран Адамян <tigranadaman@MacBook-Air-Tigran.local>
2025-05-11 23:10:27 +0300

    Доработа профиля

--- app/Models/ParsedData.php
+++ app/Models/ParsedData.php
@@ -9,5 +9,9 @@ class ParsedData extends Model
 {
     /** @use HasFactory<\Database\Factories\ParsedDataFactory> */
     use HasFactory;
-    
+    protected $fillable = ['component_id', 'source', 'price', 'availability'];
+    public function component()
+    {
+        return $this->belongsTo(Component::class, 'component_id');
+    }
 }
