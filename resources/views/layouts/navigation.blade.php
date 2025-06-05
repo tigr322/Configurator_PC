9870a93340d3ea91a11d07292d486ee808873ecf
Тигран Адамян <tigranadaman@MacBook-Air-Tigran.local>
2025-06-05 23:36:45 +0300

    Добавил фавикон

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -8,7 +8,7 @@
     
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
- 
+    <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link rel="stylesheet" href="{{ asset('css/light-dark.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
