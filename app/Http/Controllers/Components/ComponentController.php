79f3e75e2d399ac6ba42a33e69e64ab8e2648183
Тигран Адамян <tigranadaman@MacBook-Air-Tigran.local>
2025-06-19 22:00:52 +0300

    Исправление багов

--- app/Http/Controllers/Components/ComponentController.php
+++ app/Http/Controllers/Components/ComponentController.php
@@ -85,16 +85,7 @@ class ComponentController extends Controller
         $components = $query->paginate($perPage);
         $view = $request->input('view', 'grid');
         
-$components = Component::where('market_id', 4)->get();
 
-foreach ($components as $component) {
-    $words = explode(' ', $component->name);
-
-    if (isset($words[1])) {
-        $component->brand = $words[1];
-        $component->save();
-    }
-}
         $categories = Category::all();
         $rules = CompatibilityRule::all();
         $markets = Markets::all();
